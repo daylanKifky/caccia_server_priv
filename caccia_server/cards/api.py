cec4c9bcaec16e2d1f8d7838b65aa4f59ef66cd1
guiltyfeet <lorenzomf@eml.cc>
2022-01-30 00:04:24 +0100

    Push image copied from backend

--- caccia_server/cards/api.py
+++ caccia_server/cards/api.py
@@ -8,8 +8,10 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 from functools import wraps
+from os.path import join
 
 from ..db import get_db
+from .. import utils as u
 
 bp = Blueprint('api_cards', __name__, url_prefix='/cards')
 
@@ -24,10 +26,6 @@ def cards_get_dict(card_id = None):
             res = res.fetchall()
         
         else:
-            card_id = int(card_id)
-            # if not isinstance(card_id, int):
-            #     raise TypeError("card_id should be an integer")
-
             res = db.execute("SELECT * FROM cards WHERE id = ?", (card_id,))
             res = res.fetchall()
 
@@ -35,7 +33,7 @@ def cards_get_dict(card_id = None):
             response.append({k:row[k] for k in row.keys()})
         
     except Exception as e:
-        print("Error getting card data", e)
+        pass
 
     return response
 
@@ -52,10 +50,9 @@ def cards_get():
             response.append({k:row[k] for k in row.keys()})
         
     except Exception as e:
-        # err_id = u.get_error_id()
+        err_id = u.get_error_id()
 
-        # current_app.logger.error('[ Register game start error | error_id: %s ] %s\n%s---' % (err_id, e, traceback.format_exc()) )
-        err_id = 418
+        current_app.logger.error('[ Get cards list error | error_id: %s ] %s\n%s---' % (err_id, e, traceback.format_exc()) )
 
         int_error = jsonify({"status": "error", "reason": "internal error", "error_id": err_id})
         return make_response( int_error, 500 )
@@ -65,18 +62,72 @@ def cards_get():
 @bp.route('/', methods=('POST',))
 # @auth_check_dashboard(redirect_to_login=True) 
 def populate_msg():
-    
     content = request.get_json()
-    print(content)
-    db = get_db()
-    db.execute(
-            ''' UPDATE cards
-                SET image = ? ,
-                  enigmatype = ? ,
-                  question = ?,
-                  answer = ?
-                WHERE id = ?;''', (content['image'], content['enigmatype'], 
-                content['question'], content['answer'], content['id']))
-    
-    db.commit()
+
+    try:
+        db = get_db()
+        db.execute(
+                ''' UPDATE cards
+                    SET image = ? ,
+                      enigmatype = ? ,
+                      question = ?,
+                      answer = ?,
+                      modified = CURRENT_TIMESTAMP
+                    WHERE id = ?;''', (content['image'], content['enigmatype'], 
+                    content['question'], content['answer'], content['id']))
+        
+        db.commit()
+
+    except Exception as e:
+        err_id = u.get_error_id()
+
+        current_app.logger.error('[ Post cards content error | error_id: %s ] %s\n%s---' % (err_id, e, traceback.format_exc()) )
+
+        int_error = jsonify({"status": "error", "reason": "internal error", "error_id": err_id})
+        return make_response( int_error, 500 )
+
+
     return jsonify({"status": "ok"})
+
+
+@bp.route('/image/', methods=('POST',)) 
+# @auth_check_dashboard(redirect_to_login=True)
+def image(user=None):
+    try:
+        if 'file' not in request.files:
+            return jsonify({"status": "image not saved, file not present in request"})
+
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return jsonify({"status": "image not saved, empty file in request"})
+
+        if 'Content-length' not in request.headers:
+            raise u.Download_Image_Error("No Content-length header in request")
+
+        if int(request.headers['Content-length']) > current_app.config['MAX_IMG_SIZE']:
+            raise u.Download_Image_Error("The image {} is too big, max allowed {}kb".format(file.filename, current_app.config['MAX_IMG_SIZE']/1024))
+    
+        saved_image, image_path = u.save_image(file)
+        image_url = join(request.host_url, image_path)
+
+    except u.Download_Image_Error as e:
+        err_id = u.get_error_id()
+
+        int_error = jsonify({"status": "error", "reason": "Image upload error: {}".format(e), "error_id": err_id})
+        current_app.logger.error('[ Post cards image Upload error | error_id: %s ] %s\n%s---' % (err_id, e, traceback.format_exc()) )
+
+        return make_response( int_error, 500 )
+
+
+    except Exception as e:
+        err_id = u.get_error_id()
+
+        current_app.logger.error('[ Post cards generic image error | error_id: %s ] %s\n%s---' % (err_id, e, traceback.format_exc()) )
+
+        int_error = jsonify({"status": "error", "reason": "internal error", "error_id": err_id})
+        return make_response( int_error, 500 )
+    
+
+    return jsonify({"status": "ok", "saved_image": saved_image, "image_url": image_url})
